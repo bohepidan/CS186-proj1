0f44f3feeee283a96ff3ae38251e18cc86509096
bottle <laolai@LAPTOP-K3TR9NG2.localdomain>
2022-11-12 17:37:51 +0800

    proj1 init

--- proj1.sql
+++ proj1.sql
@@ -19,67 +19,119 @@ DROP VIEW IF EXISTS q4v;
 -- Question 0
 CREATE VIEW q0(era)
 AS
-  SELECT 1 -- replace this line
+  SELECT MAX(era)
+  FROM pitching
 ;
 
 -- Question 1i
 CREATE VIEW q1i(namefirst, namelast, birthyear)
 AS
-  SELECT 1, 1, 1 -- replace this line
+  SELECT namefirst, namelast, birthyear
+  FROM  people p
+  WHERE p.weight > 300
 ;
 
 -- Question 1ii
 CREATE VIEW q1ii(namefirst, namelast, birthyear)
 AS
-  SELECT 1, 1, 1 -- replace this line
+  SELECT namefirst, namelast, birthyear
+  FROM people p
+  WHERE p.namefirst LIKE '% %'
+  ORDER BY namefirst, namelast
 ;
 
 -- Question 1iii
 CREATE VIEW q1iii(birthyear, avgheight, count)
 AS
-  SELECT 1, 1, 1 -- replace this line
+  SELECT birthyear, AVG(height), COUNT(*)
+  FROM people p 
+  GROUP BY birthyear
+  ORDER BY birthyear
 ;
 
 -- Question 1iv
 CREATE VIEW q1iv(birthyear, avgheight, count)
 AS
-  SELECT 1, 1, 1 -- replace this line
+  SELECT birthyear, AVG(height), COUNT(*)
+  FROM people p 
+  GROUP BY birthyear
+  HAVING AVG(height) > 70
+  ORDER BY birthyear
 ;
 
 -- Question 2i
 CREATE VIEW q2i(namefirst, namelast, playerid, yearid)
 AS
-  SELECT 1, 1, 1, 1 -- replace this line
+  SELECT namefirst, namelast, p.playerid, yearid
+  FROM people p INNER JOIN hallOfFame h
+  ON p.playerid = h.playerid and inducted = 'Y'
+  ORDER BY yearid DESC, p.playerid
 ;
 
 -- Question 2ii
 CREATE VIEW q2ii(namefirst, namelast, playerid, schoolid, yearid)
 AS
-  SELECT 1, 1, 1, 1, 1 -- replace this line
+  SELECT namefirst, namelast, p.playerid, s.schoolid, yearid
+  FROM people p, halloffame h, collegeplaying c, schools s
+  WHERE p.playerid = h.playerid and p.playerid = c.playerid 
+    and s.schoolid = c.schoolid and s.schoolState = 'CA' and h.inducted = 'Y'
+  ORDER BY h.yearid DESC, s.schoolid, p.playerid
 ;
 
 -- Question 2iii
 CREATE VIEW q2iii(playerid, namefirst, namelast, schoolid)
 AS
-  SELECT 1, 1, 1, 1 -- replace this line
+  SELECT p.playerid, namefirst, namelast, s.schoolid
+  FROM (people p INNER JOIN halloffame h 
+  ON p.playerid = h.playerid and h.inducted = 'Y' )
+  LEFT OUTER JOIN (
+    collegeplaying c INNER JOIN schools s 
+    ON c.schoolid = s.schoolid
+  )ON p.playerid = c.playerid
+  ORDER BY p.playerid DESC, s.schoolid 
 ;
 
 -- Question 3i
+CREATE VIEW slg(playerid, yearid, AB, slgval)
+AS 
+  SELECT playerid, yearid, AB, (H + H2B + 2*H3B + 3*HR + 0.0)/(AB + 0.0)
+  FROM batting
+;
+
 CREATE VIEW q3i(playerid, namefirst, namelast, yearid, slg)
 AS
-  SELECT 1, 1, 1, 1, 1 -- replace this line
+  SELECT p.playerid, p.namefirst, p.namelast, s.yearid, s.slgval
+  FROM people p INNER JOIN slg s
+  ON p.playerid = s.playerid
+  WHERE s.AB > 50
+  ORDER BY s.slgval DESC, s.yearid, p.playerid
+  LIMIT 10
 ;
 
 -- Question 3ii
 CREATE VIEW q3ii(playerid, namefirst, namelast, lslg)
 AS
-  SELECT 1, 1, 1, 1 -- replace this line
+  SELECT p.playerid, p.namefirst, p.namelast, (sum(b.H)+sum(b.H2B)+sum(b.H3B)*2+sum(b.HR)*3+0.0)/sum(b.AB) as lslg
+  FROM people p INNER JOIN batting b
+  ON p.playerid = b.playerid 
+  GROUP BY p.playerid
+  HAVING sum(b.AB) > 50
+  ORDER BY lslg DESC, p.playerid
+  LIMIT 10
 ;
 
 -- Question 3iii
 CREATE VIEW q3iii(namefirst, namelast, lslg)
 AS
-  SELECT 1, 1, 1 -- replace this line
+  SELECT p.namefirst, p.namelast, (sum(b.H)+sum(b.H2B)+sum(b.H3B)*2+sum(b.HR)*3+0.0)/(sum(b.AB)+0.0) 
+  FROM people p INNER JOIN batting b
+  ON p.playerid = b.playerid
+  GROUP BY p.playerid, p.namefirst, p.namelast
+  HAVING  > (
+    SELECT (sum(b.H)+sum(b.H2B)+sum(b.H3B)*2+sum(b.HR)*3+0.0)/(sum(b.AB)+0.0)
+    FROM people p INNER JOIN batting b
+    ON p.playerid = b.playerid and p.playerid = 'mayswi01'
+  )
 ;
 
 -- Question 4i
